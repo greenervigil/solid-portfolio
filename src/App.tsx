a1f5e6de59c37ef28b744d86875f99e3f3fa6b83
greenervigil <daniel.julian.vigil@gmail.com>
2022-11-27 21:55:08 -0500

    set up header with minimal styling to start

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import type { Component } from 'solid-js';
 const App: Component = () => {
   return (
     <>
-    <nav>
-      <ul class="flex flex-row justify-end px-6">
+    <nav class="flex flex-row justify-between">
+      <a class="p-6" href="/">
+        <p class="text-gray-800 text-2xl">Daniel J Greener-Vigil</p>
+      </a>
+      <ul class="flex flex-row">
         <li class='p-6'>Home</li>
         <li class="p-6">About</li>
+        <li class="p-6">Experience</li>
+        <li class="p-6">Projects</li>
+        <li class="p-6">Contact</li>
       </ul>
     </nav>
 
