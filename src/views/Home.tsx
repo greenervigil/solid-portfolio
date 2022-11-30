22a96f1c8e02233ee5e884f1a67182ba2ed3ec25
greenervigil <daniel.julian.vigil@gmail.com>
2022-11-29 20:57:04 -0500

    Updating design to home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,12 +3,15 @@ import Button from "../components/Button";
 
 const Home: Component = () => {
     return (
-        <div class='bg-cyan-800 h-screen px-10 flex flex-col justify-center'>
-            <div class='text-sm text-cyan-300'>Hi, my name is</div>
-            <div class='text-6xl text-zinc-300 p-3'>Daniel Greener-Vigil</div>
-            <div class='text-slate-400 text-base p-3'>I'm a Software Engineer focus on developing full stack digital solutions. </div>
+        <div class="bg-cyan-800 h-screen py-48">
+            <div class="bg-yellow-600 absolute -my-36 mx-96 rounded-full h-96 w-96"></div>
+            <div class='bg-gray-500 px-10 w-3/5 rounded-lg shadow-2xl opacity-50 py-5 z-10'>
+                <div class='text-sm text-cyan-300 opacity-100'>Hi, my name is</div>
+                <div class='text-6xl text-zinc-300 p-3 opacity-100'>Daniel Greener-Vigil</div>
+                <div class='text-zinc-300 text-base p-3 opacity-100'>I'm a Software Engineer focus on developing full stack digital solutions. </div>
 
-            <Button text="View My Work" />
+                <Button text="View My Work" />
+            </div>
         </div>
     )
 }
