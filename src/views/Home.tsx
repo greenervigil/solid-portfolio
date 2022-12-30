d5fb4c3504698f1be3b13e7ffa32556b6883aa71
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-12-29 21:32:53 -0500

    minor updates

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -10,7 +10,7 @@ const Home: Component = () => {
     return (
         <div class="bg-cyan-800 h-screen py-48">
             <div class="bg-yellow-600 absolute shadow-2xl -my-36 mx-96 rounded-full h-96 w-96"></div>
-            <div class='bg-gray-500 px-10 xs:w-2 md:w-6/12 rounded-lg shadow-2xl backdrop-blur-3xl opacity-70 py-5'>
+            <div class='bg-gray-500 m-7 px-10 py-5 xs:w-2 md:w-6/12 rounded-lg shadow-2xl backdrop-blur-3xl opacity-70'>
                 <div class='blur-0'>
                     <div class='text-sm text-cyan-300'>Hi, my name is</div>
                     <div class='text-6xl text-zinc-300 p-3'>Daniel Greener-Vigil</div>
